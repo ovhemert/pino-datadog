cadae76439e6d8c583b6ac2032ded0856a7cc073
Nicolas Djambazian <nicolas@djambazian.fr>
2019-10-28 18:08:14 +0100

    Don't stop the pipe on error

--- src/datadog.js
+++ src/datadog.js
@@ -32,7 +32,8 @@ class Client {
       const result = await axios.post(url, data, { params })
       return result
     } catch (err) {
-      throw Error(err.message)
+      console.error('The previous log have not been saved')
+      console.error(err.stack || err)
     }
   }
 
