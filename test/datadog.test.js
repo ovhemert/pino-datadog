74d84c5ce69b45a7be6f2eb4624f7f3bee538b69
Nicolas Djambazian <nicolas@djambazian.fr>
2019-10-28 10:29:34 +0100

    Update tests

--- test/datadog.test.js
+++ test/datadog.test.js
@@ -6,12 +6,6 @@ const sinon = require('sinon')
 
 const axios = require('axios')
 
-test('creates client', t => {
-  const client = new tested.Client({ apiKey: '1234567890' })
-  t.equals(client._apiKey, '1234567890')
-  t.end()
-})
-
 test('calls insert without document', t => {
   const client = new tested.Client()
   client.insert().then(data => {
@@ -46,14 +40,20 @@ test('inserts single document', t => {
 test('inserts multiple documents', t => {
   const client = new tested.Client()
   const stubPost = sinon.stub(axios, 'post').resolvesArg(1)
-  client.insert([{ message: 'test 1' }, { message: 'test 2' }, { message: 'test 3' }]).then(data => {
-    t.equals(data.length, 3)
-    t.equals(data[0].message, 'test 1')
-    t.equals(data[1].message, 'test 2')
-    t.equals(data[2].message, 'test 3')
-    stubPost.restore()
-    t.end()
-  })
+  client
+    .insert([
+      { message: 'test 1' },
+      { message: 'test 2' },
+      { message: 'test 3' }
+    ])
+    .then(data => {
+      t.equals(data.length, 3)
+      t.equals(data[0].message, 'test 1')
+      t.equals(data[1].message, 'test 2')
+      t.equals(data[2].message, 'test 3')
+      stubPost.restore()
+      t.end()
+    })
 })
 
 test('inserts with write stream', t => {
@@ -63,5 +63,71 @@ test('inserts with write stream', t => {
   ws.write({ message: 'test 1' })
   ws.end()
   t.ok(stubPost.called)
+  stubPost.restore()
+  t.end()
+})
+
+test('inserts sends com url and api key', async t => {
+  const client = new tested.Client({ apiKey: '1234567890' })
+  const stubPost = sinon.stub(axios, 'post')
+  const items = [{ message: 'hello world !' }]
+
+  await client.insert(items)
+  t.ok(stubPost.called)
+  t.ok(
+    stubPost.calledWithMatch(
+      'https://http-intake.logs.datadoghq.com/v1/input/1234567890',
+      items,
+      { params: {} }
+    )
+  )
+  stubPost.restore()
+  t.end()
+})
+
+test('inserts sends eu url and api key', async t => {
+  const client = new tested.Client({ apiKey: '1234567890', eu: true })
+  const stubPost = sinon.stub(axios, 'post')
+  const items = [{ message: 'hello world !' }]
+
+  await client.insert(items)
+  t.ok(stubPost.called)
+  t.ok(
+    stubPost.calledWithMatch(
+      'https://http-intake.logs.datadoghq.eu/v1/input/1234567890',
+      items,
+      { params: {} }
+    )
+  )
+  stubPost.restore()
+  t.end()
+})
+
+test('inserts sends extra parameters ', async t => {
+  const client = new tested.Client({
+    apiKey: '1234567890',
+    ddsource: 'source',
+    service: 'service',
+    hostname: 'foobar.com'
+  })
+  const stubPost = sinon.stub(axios, 'post')
+  const items = [{ message: 'hello world !' }]
+
+  await client.insert(items)
+  t.ok(stubPost.called)
+  t.ok(
+    stubPost.calledWithMatch(
+      'https://http-intake.logs.datadoghq.com/v1/input/1234567890',
+      items,
+      {
+        params: {
+          ddsource: 'source',
+          service: 'service',
+          hostname: 'foobar.com'
+        }
+      }
+    )
+  )
+  stubPost.restore()
   t.end()
 })
